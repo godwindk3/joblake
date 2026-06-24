af2504380cab547af6556af302d843a8584ccc28
godwindk3 <phongprosupper@gmail.com>
2026-06-24 11:41:21 +0700

    test playwright

---
+++ src/ingestion/test_crawl_with_playwright_and_bs4.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(
+        headless=True,
+        args=["--disable-blink-features=AutomationControlled"]
+        )
+
+    context = browser.new_context(
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    )
+    
+    # page = browser.new_page()
+
+    page = context.new_page()
+
+
+    page.goto("https://www.topcv.vn/viec-lam/data-scientist/2147404.html?ta_source=JobSearchList_LinkDetail&u_sr_id=KbcA7ex7ZBBeeJSgvIcfgFgjLcHRrsxz9XW816Es_1782268004")
+
+
+    html = page.content()
+
+    print(html)
+
+    browser.close()
+
+
+
+# soup = BeautifulSoup(html, "html.parser")
+# print(soup.select_one(".job-detail__info--section-content-value"))
+# print(page.title())
